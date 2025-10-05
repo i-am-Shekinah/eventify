2f4f1a2caff3c1871f30b33083ccd05833398652
Michael <MichaelOlatunji.dev@gmail.com>
2025-10-05 18:45:04 +0100

    initial commit - include Event model

---
+++ eventify/src/main/java/com/codewithmike/eventify/event/Event.java
@@ -0,0 +1,28 @@
+package com.codewithmike.eventify.event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String title;
+    private String description;
+    private String location;
+    private LocalDateTime date;
+}
